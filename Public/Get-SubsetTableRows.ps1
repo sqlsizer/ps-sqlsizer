d6a00db3fb24235be19c95b4bf400da23f16af2a
Marcin Golebiowski <xormus@gmail.com>
2022-05-09 21:03:52 +0200

    Add $AllColumns parameter to Get-SubsetTableRows

--- Public/Get-SubsetTableRows.ps1
+++ Public/Get-SubsetTableRows.ps1
@@ -12,6 +12,9 @@ function Get-SubsetTableRows
         [Parameter(Mandatory=$true)]
         [string]$TableName,
 
+        [Parameter(Mandatory=$false)]
+        [bool]$AllColumns = $false,
+
         [Parameter(Mandatory=$true)]
         [SqlConnectionInfo]$ConnectionInfo
     )
@@ -23,22 +26,54 @@ function Get-SubsetTableRows
     {
         if (($table.SchemaName -eq $SchemaName) -and ($table.TableName -eq $TableName))
         {
-            $tableName = $structure.GetProcessingName($structure.Tables[$table])
+            $processing = $structure.GetProcessingName($structure.Tables[$table])
 
-            $keys = ""
-            for ($i = 0; $i -lt $table.PrimaryKey.Count; $i++)
+            if ($AllColumns -eq $false)
             {
-                $keys += "Key$($i) as $($table.PrimaryKey[$i].Name)"
-
-                if ($i -lt ($table.PrimaryKey.Count - 1))
+                $keys = ""
+                for ($i = 0; $i -lt $table.PrimaryKey.Count; $i++)
                 {
-                    $keys += ", "
-                }
-            }
+                    $keys += "Key$($i) as $($table.PrimaryKey[$i].Name)"
 
-            $sql = "SELECT DISTINCT '$($table.TableName)' as SchemaName,'$($table.SchemaName)' as TableName, $($keys) FROM $($tableName) WHERE [Schema] = '$($table.SchemaName)' AND TableName = '$($table.TableName)'"
-            $rows = Execute-SQL -Sql $sql -Database $Database -ConnectionInfo $ConnectionInfo 
-            return $rows
+                    if ($i -lt ($table.PrimaryKey.Count - 1))
+                    {
+                        $keys += ", "
+                    }
+                }
+
+                $sql = "SELECT DISTINCT '$($table.TableName)' as SchemaName,'$($table.SchemaName)' as TableName, $($keys) FROM $($processing) WHERE [Schema] = '$($table.SchemaName)' AND TableName = '$($table.TableName)'"
+                $rows = Execute-SQL -Sql $sql -Database $Database -ConnectionInfo $ConnectionInfo 
+                return $rows
+            }
+            else
+            {
+                $columns = ""
+
+                for ($i = 0; $i -lt $table.Columns.Count; $i++)
+                {
+                    $columns += "t.$($table.Columns[$i].Name)"
+                    if ($i -lt ($table.Columns.Count - 1))
+                    {
+                        $columns += ", "
+                    }
+                }
+
+                $cond = ""
+                for ($i = 0; $i -lt $table.PrimaryKey.Count; $i++)
+                {
+                    $cond += "t.$($table.PrimaryKey[$i].Name) = p.Key$($i)"
+                    if ($i -lt ($table.PrimaryKey.Count - 1))
+                    {
+                        $cond += " and "
+                    }
+                }
+                $sql = "SELECT '$($table.TableName)' as SchemaName, '$($table.SchemaName)' as TableName, $($columns)
+                        FROM $($processing) p 
+                        INNER JOIN $($table.SchemaName).$($table.TableName) t ON $($cond)
+                        WHERE p.[Schema] = '$($table.SchemaName)' AND p.TableName = '$($table.TableName)'"
+                $rows = Execute-SQL -Sql $sql -Database $Database -ConnectionInfo $ConnectionInfo 
+                return $rows
+            }
         }
     }
 
