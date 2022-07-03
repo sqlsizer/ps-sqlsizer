a3df45b1e0f327e3826184580cec26413737d30d
Marcin Golebiowski <xormus@gmail.com>
2022-07-03 22:27:17 +0200

    Add Copy-FileToDatabase and Remove-FileFromDatabase

---
+++ MSSQL-SqlSizer/Public/Copy-FileToDatabase.ps1
@@ -0,0 +1,45 @@
+function Copy-FileToDatabase
+{
+    [cmdletbinding()]
+    [outputtype([System.Boolean])]
+    param
+    (
+        [Parameter(Mandatory=$true)]
+        [string]$Database,
+
+        [Parameter(Mandatory=$true)]
+        [string]$FilePath,
+
+        [Parameter(Mandatory=$true)]
+        [SqlConnectionInfo]$ConnectionInfo
+    )
+
+    $id = New-Guid
+    $fileContent = [System.IO.File]::ReadAllText($FilePath)
+
+    $chunkSize = 2000
+    $chunks = [Math]::Floor($fileContent.Length / $chunkSize)
+
+    for ($i = 0; $i -lt $chunks; $i = $i + 1)
+    {
+        Write-Progress -Activity "Copying file $FilePath to database" -PercentComplete (100 * ($i / ($chunks)))
+
+        $chunk = $fileContent.Substring($i * $chunkSize, $chunkSize)
+        $sql = "INSERT INTO SqlSizer.Files([FileId], [Index], [Content]) VALUES('$id', $i, N'$chunk')"
+        $null = Invoke-SqlcmdEx -Sql $sql -Database $Database -ConnectionInfo $ConnectionInfo
+    }
+
+    if ($chunks * $chunkSize -ne $fileContent.Length)
+    {
+        $chunk = $fileContent.Substring($chunks * $chunkSize)
+        
+        $chunk = $chunk.Replace("'", "''") # improve it 
+
+        $sql = "INSERT INTO SqlSizer.Files([FileId], [Index], [Content]) VALUES('$id', $i, N'$chunk')"
+        $null = Invoke-SqlcmdEx -Sql $sql -Database $Database -ConnectionInfo $ConnectionInfo
+    }
+
+    Write-Progress -Activity "Copying file $FilePath to database" -Completed
+
+    return $id
+}
---
+++ MSSQL-SqlSizer/Public/Remove-FileFromDatabase.ps1
@@ -0,0 +1,19 @@
+function Remove-FileFromDatabase
+{
+    [cmdletbinding()]
+    [outputtype([System.Boolean])]
+    param
+    (
+        [Parameter(Mandatory=$true)]
+        [string]$Database,
+
+        [Parameter(Mandatory=$true)]
+        [string]$FileId,
+
+        [Parameter(Mandatory=$true)]
+        [SqlConnectionInfo]$ConnectionInfo
+    )
+
+    $sql = "DELETE FROM SqlSizer.Files WHERE [FileId] = '$FileId'"
+    $null = Invoke-SqlcmdEx -Sql $sql -Database $Database -ConnectionInfo $ConnectionInfo
+}
