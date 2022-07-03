dac9d9ba05b321f535e502334ca74c59b9fa3598
Marcin Golebiowski <xormus@gmail.com>
2022-07-03 20:55:36 +0200

    Improve Uninstall-SqlSizer

--- MSSQL-SqlSizer/Public/Uninstall-SqlSizer.ps1
+++ MSSQL-SqlSizer/Public/Uninstall-SqlSizer.ps1
@@ -13,72 +13,8 @@
         [SqlConnectionInfo]$ConnectionInfo
     )
 
-    $info = Get-DatabaseInfoIfNull -Database $Database -Connection $ConnectionInfo -DatabaseInfo $DatabaseInfo
-
-    $tmp = "IF OBJECT_ID('SqlSizer.Operations') IS NOT NULL
-    DROP TABLE SqlSizer.Operations"
-    Invoke-SqlcmdEx -Sql $tmp -Database $Database -ConnectionInfo $ConnectionInfo
-
-    $tmp = "IF OBJECT_ID('SqlSizer.Tables') IS NOT NULL
-    DROP TABLE SqlSizer.Tables"
-    Invoke-SqlcmdEx -Sql $tmp -Database $Database -ConnectionInfo $ConnectionInfo
-
-    $tmp = "IF OBJECT_ID('SqlSizer.ForeignKeys') IS NOT NULL
-    DROP TABLE SqlSizer.ForeignKeys"
-    Invoke-SqlcmdEx -Sql $tmp -Database $Database -ConnectionInfo $ConnectionInfo
-
-    $structure = [Structure]::new($info)
-
-    foreach ($signature in $structure.Signatures.Keys)
-    {
-        $slice = $structure.GetSliceName($signature)
-        $tmp = "IF OBJECT_ID('$($slice)') IS NOT NULL
-        DROP TABLE $($slice)"
-        Invoke-SqlcmdEx -Sql $tmp -Database $Database -ConnectionInfo $ConnectionInfo
-    }
-
-    foreach ($signature in $structure.Signatures.Keys)
-    {
-        $processing = $structure.GetProcessingName($signature)
-        $tmp = "IF OBJECT_ID('$($processing)') IS NOT NULL
-        DROP TABLE $($processing)"
-        Invoke-SqlcmdEx -Sql $tmp -Database $Database -ConnectionInfo $ConnectionInfo
-    }
-
-    foreach ($table in $info.Tables)
-    {
-        $tmp = "IF OBJECT_ID('SqlSizerResult.$($table.SchemaName)_$($table.TableName)', 'V') IS NOT NULL
-        DROP VIEW SqlSizerResult.$($table.SchemaName)_$($table.TableName)"
-        Invoke-SqlcmdEx -Sql $tmp -Database $Database -ConnectionInfo $ConnectionInfo
-    }
-
-    foreach ($table in $info.Tables)
-    {
-        $tmp = "IF OBJECT_ID('SqlSizerSecure.$($table.SchemaName)_$($table.TableName)', 'V') IS NOT NULL
-        DROP VIEW SqlSizerSecure.$($table.SchemaName)_$($table.TableName)"
-        Invoke-SqlcmdEx -Sql $tmp -Database $Database -ConnectionInfo $ConnectionInfo
-    }
-
-    foreach ($table in $info.Tables)
-    {
-        $tmp = "IF OBJECT_ID('SqlSizerExport.$($table.SchemaName)_$($table.TableName)', 'V') IS NOT NULL
-        DROP VIEW SqlSizerExport.$($table.SchemaName)_$($table.TableName)"
-        Invoke-SqlcmdEx -Sql $tmp -Database $Database -ConnectionInfo $ConnectionInfo
-    }
-
-    $tmp = "IF OBJECT_ID('SqlSizerSecure.Summary', 'V') IS NOT NULL
-        DROP VIEW SqlSizerSecure.Summary"
-    Invoke-SqlcmdEx -Sql $tmp -Database $Database -ConnectionInfo $ConnectionInfo
-
-    $tmp = "DROP SCHEMA IF EXISTS SqlSizer"
-    $null = Invoke-SqlcmdEx -Sql $tmp -Database $Database -ConnectionInfo $ConnectionInfo
-
-    $tmp = "DROP SCHEMA IF EXISTS SqlSizerResult"
-    $null = Invoke-SqlcmdEx -Sql $tmp -Database $Database -ConnectionInfo $ConnectionInfo
-
-    $tmp = "DROP SCHEMA IF EXISTS SqlSizerSecure"
-    $null = Invoke-SqlcmdEx -Sql $tmp -Database $Database -ConnectionInfo $ConnectionInfo
-
-    $tmp = "DROP SCHEMA IF EXISTS SqlSizerExport"
-    $null = Invoke-SqlcmdEx -Sql $tmp -Database $Database -ConnectionInfo $ConnectionInfo
+    Remove-Schema -Database $Database -SchemaName "SqlSizerResult" -ConnectionInfo $ConnectionInfo
+    Remove-Schema -Database $Database -SchemaName "SqlSizerSecure" -ConnectionInfo $ConnectionInfo
+    Remove-Schema -Database $Database -SchemaName "SqlSizerExport" -ConnectionInfo $ConnectionInfo
+    Remove-Schema -Database $Database -SchemaName "SqlSizer" -ConnectionInfo $ConnectionInfo
 }
