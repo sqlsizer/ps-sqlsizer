a4e39fb4732955b5e7ba0783f2508fad202a8aa2
Marcin Golebiowski <xormus@gmail.com>
2022-07-05 22:59:57 +0200

    Add Remove-DataFromFileSet

---
+++ MSSQL-SqlSizer/Public/Remove-DataFromFileSet.ps1
@@ -0,0 +1,62 @@
+﻿function Remove-DataFromFileSet
+{
+    [cmdletbinding()]
+    param
+    (
+        [Parameter(Mandatory=$true)]
+        [string]$Database,
+
+        [Parameter(Mandatory=$true)]
+        [TableFile[]]$Files,
+
+        [Parameter(Mandatory=$false)]
+        [DatabaseInfo]$DatabaseInfo = $null,
+
+        [Parameter(Mandatory=$true)]
+        [SqlConnectionInfo]$ConnectionInfo
+    )
+
+    $info = Get-DatabaseInfoIfNull -Database $Database -Connection $ConnectionInfo -DatabaseInfo $DatabaseInfo
+    
+    foreach ($file in $Files)
+    {
+        $tableInfo = $info.Tables | Where-Object { ($_.SchemaName -eq $file.TableContent.SchemaName) -and ($_.TableName -eq $file.TableContent.TableName) }
+        $where = @()
+        foreach ($column in $tableInfo.PrimaryKey)
+        {
+           $where += "t.$($column.Name) = o.$($column.Name)"
+        }
+
+        $columns = @()
+        foreach ($column in $tableInfo.Columns)
+        {
+            if ($column.IsComputed)
+            {
+                continue
+            }
+
+            if ($column.DataType -in @('geography', 'hierarchyid', 'xml'))
+            {
+                $type = 'varchar(max)'
+            }
+            else
+            {
+                $type = $column.DataType
+                
+                if ($type -in @('nvarchar', 'varchar'))
+                {
+                    $type += "(max)"
+                }
+            }
+
+            $columns += "[" + $column.Name + "] " + $type
+        }
+   
+        $sql = "DECLARE @json NVARCHAR(MAX); SELECT @json = STRING_AGG([Content], '') FROM SqlSizer.Files WHERE [FileId] = '$($file.FileId)'
+                
+                DELETE t
+                FROM OpenJson(@json) with ($([string]::join(', ', $columns)))  as o
+                INNER JOIN [$($tableInfo.SchemaName)].[$($tableInfo.TableName)] t ON $([string]::join(' and ', $where))"
+        $null = Invoke-SqlcmdEx -Sql $sql -Database $Database -ConnectionInfo $ConnectionInfo
+    }
+}
