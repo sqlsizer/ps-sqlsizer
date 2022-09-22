736cd019569aa11ff0cbcebd6ba60469905a584f
Marcin Golebiowski <xormus@gmail.com>
2022-09-22 22:17:43 +0200

    Add Get-SqlSizerInfo

---
+++ SqlSizer-MSSQL/Public/Get-SqlSizerInfo.ps1
@@ -0,0 +1,50 @@
+function Get-SqlSizerInfo
+{
+    [cmdletbinding()]
+    param
+    (
+        [Parameter(Mandatory=$true)]
+        [string]$Database,
+
+        [Parameter(Mandatory=$true)]
+        [SqlConnectionInfo]$ConnectionInfo
+    )
+
+    $sql = "SELECT [Id],[Schema],[TableName] FROM [SqlSizer].[Tables]"
+    $tablesRows = Invoke-SqlcmdEx -Sql $sql -Database $Database -ConnectionInfo $ConnectionInfo
+    
+    $sql = "SELECT  
+        f.[Id]
+        ,[FkTableId]
+        ,[TableId]
+        ,[Name]
+		,ft.[Schema]
+		,ft.[TableName]
+        FROM [SqlSizer].[ForeignKeys] f
+	INNER JOIN [SqlSizer].[Tables] ft ON f.FkTableId = ft.Id"
+    $fkRows = Invoke-SqlcmdEx -Sql $sql -Database $Database -ConnectionInfo $ConnectionInfo
+
+    $tables = $tablesRows | ForEach-Object {
+        [pscustomobject] @{
+            Id = $_.Id
+            SchemaName = $_.Schema
+            TableName = $_.TableName
+        }
+    }
+
+    $fks = $fkRows  | ForEach-Object {
+        [pscustomobject] @{
+            Id = $_.Id
+            Name = $_.Name
+            FkSchemaName = $_.Schema
+            FkTableName = $_.TableName
+        }
+    }
+
+    $result = [pscustomobject]@{
+        Tables = $tables
+        ForeignKeys = $fks
+    }
+    
+    return $result
+}
