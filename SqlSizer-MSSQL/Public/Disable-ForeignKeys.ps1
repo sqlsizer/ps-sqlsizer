edee043dcb95139d6803514dfd000e50ddface46
Marcin Golebiowski <xormus@gmail.com>
2022-09-25 09:16:56 +0200

    Fix comments in Disable-ForeignKeys

--- SqlSizer-MSSQL/Public/Disable-ForeignKeys.ps1
+++ SqlSizer-MSSQL/Public/Disable-ForeignKeys.ps1
@@ -13,17 +13,17 @@
         [SqlConnectionInfo]$ConnectionInfo
     )
 
-    Write-Progress -Activity "Disabling integrity checks on database" -PercentComplete 0
+    Write-Progress -Activity "Disabling foreign key checks on database" -PercentComplete 0
     $i = 0
     foreach ($table in $DatabaseInfo.Tables)
     {
         $i += 1
-        Write-Progress -Activity "Disabling integrity checks on database" -PercentComplete (100 * ($i / $DatabaseInfo.Tables.Count))
+        Write-Progress -Activity "Disabling foreign key checks on database" -PercentComplete (100 * ($i / $DatabaseInfo.Tables.Count))
 
         $sql = "ALTER TABLE " + $table.SchemaName + "." + $table.TableName + " NOCHECK CONSTRAINT ALL"
         $null = Invoke-SqlcmdEx -Sql $sql -Database $Database -ConnectionInfo $ConnectionInfo
     }
-    Write-Progress -Activity "Disabling integrity checks on database" -Completed
+    Write-Progress -Activity "Disabling foreign key checks on database" -Completed
 }
 # SIG # Begin signature block
 # MIIoigYJKoZIhvcNAQcCoIIoezCCKHcCAQExDzANBglghkgBZQMEAgEFADB5Bgor
