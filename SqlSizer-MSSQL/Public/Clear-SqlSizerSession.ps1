d20e311fbb922a5a5643f737b7795acfb1335e3b
Marcin Golebiowski <xormus@gmail.com>
2022-12-03 22:04:17 +0100

    Fix a bug in Clear-SqlSizerSession

--- SqlSizer-MSSQL/Public/Clear-SqlSizerSession.ps1
+++ SqlSizer-MSSQL/Public/Clear-SqlSizerSession.ps1
@@ -18,7 +18,7 @@ function Clear-SqlSizerSession
     # remove session
     Write-Verbose "SqlSizer: Remove session: $SessionId"
 
-    Update-DatabaseInfo -DatabaseInfo $info -Database $Database -ConnectionInfo $connection
+    Update-DatabaseInfo -DatabaseInfo $DatabaseInfo -Database $Database -ConnectionInfo $ConnectionInfo
     Remove-Schema -Database $Database -SchemaName "SqlSizer_$SessionId" -ConnectionInfo $ConnectionInfo -DatabaseInfo $DatabaseInfo -DropFks $false
     
     $sql = "DELETE FROM SqlSizer.Sessions WHERE SessionId = '$SessionId'"
